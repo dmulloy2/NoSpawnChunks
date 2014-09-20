ddfdf9919d2e14c30cbc285758aab8b57aaa82fc
Dan Mulloy <dmulloy2@live.com>
2014-09-20 17:27:45 -0400

    Actually load keepSpawnInMemory

--- src/main/java/net/dmulloy2/nospawnchunks/NoSpawnChunks.java
+++ src/main/java/net/dmulloy2/nospawnchunks/NoSpawnChunks.java
@@ -1,5 +1,19 @@
 /**
- * (c) 2014 dmulloy2
+ * NoSpawnChunks - a Bukkit plugin
+ * Copyright (C) - 2014 dmulloy2
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package net.dmulloy2.nospawnchunks;
 
@@ -109,6 +123,7 @@ public class NoSpawnChunks extends JavaPlugin
 		autoEnabled = getConfig().getBoolean("task.enabled");
 		allWorlds = worlds.isEmpty() || worlds.contains("*");
 		interval = getConfig().getInt("task.interval", 15) * 60 * 20;
+		keepSpawnInMemory = getConfig().getBoolean("keepSpawnInMemory", false);
 
 		garbageCollectorTask = getConfig().getBoolean("garbageCollector.task");
 		garbageCollectorUnloading = getConfig().getBoolean("garbageCollector.unloading");
